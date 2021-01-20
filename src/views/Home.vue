a645caf834c44f4bde64bbbe687204fe8c2d4600
Lok <you@example.com>
2021-01-20 16:34:52 +0800

    router setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-app>
+  <v-app-bar
+  app
+  color=blue
+  >
+    <span
+    style="margin: auto">
+    DonQ
+    </span>
+  </v-app-bar>
+  <v-main>
+    <v-text-field
+    label="Enter RestaurantId Here"
+    :rules="rules"
+    hide-details="auto"
+    v-model="RestaurantId"
+    >
+    </v-text-field>
+    <v-btn
+    rounded
+    color="primary"
+    @click="ToRestaurantPage">
+    Go
+    </v-btn>
+  </v-main>
+</v-app>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+    data: () => ({
+      rules: [
+        value => !!value || 'Required.',
+        value => {
+          const pattern = /^[0-9]\d*$|^$/
+          return pattern.test(value) || 'Numbers Only'
+        }
+      ],
+      Code:"",
+      RestaurantId:"",
+    }),
+    methods: {
+      ToRestaurantPage(){
+        this.$router.push({ name: 'RestaurantPage', params: { RestaurantId: this.RestaurantId}})
+      }
+    }
   }
-}
 </script>
---
+++ src/views/RestaurantPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>
+    this is page {{RestaurantId}}
+    </h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            RestaurantId: this.$route.params.RestaurantId,
+            }
+    }
+}
+</script>
